32a386b46391b54a7ec82ea16f4c3ed8ea019d6d
Mertion <li_zhiguoxq@163.com>
2022-08-01 17:24:09 +0800

    首次提交

---
+++ C# 画图模板代码示例/02-控件放大、缩小/Wp 控件拖动调整大小/Wp 控件拖动调整大小/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wp_控件拖动调整大小
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ C# 画图模板代码示例/03-控件旋转、放大、缩小/Wpf控件旋转/Wpf控件旋转/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Wpf控件旋转
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs args)
+        {
+            CheckBox selectionCheckBox = sender as CheckBox;
+            if (selectionCheckBox != null && selectionCheckBox.IsChecked == true)
+            {
+                foreach (Control child in DesignerCanvas.Children)
+                {
+                    Selector.SetIsSelected(child, true);
+                }
+            }
+            else
+            {
+                foreach (Control child in DesignerCanvas.Children)
+                {
+                    Selector.SetIsSelected(child, false);
+                }
+            }
+        }
+    }
+}
